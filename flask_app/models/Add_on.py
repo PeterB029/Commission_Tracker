5c7d607912b71a94964addc52b9c9dfa1043df8b
Peter Bui <peter.bui029@gmail.com>
2023-02-22 19:21:20 -0500

    Restructured Files. Renamed Commissions to Orders, and Added Templates.

--- flask_app/models/Add_on.py
+++ flask_app/models/Add_on.py
@@ -1,1 +1,21 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+
+db = 'commissions_schema'
+
+class Add_on:
+    def __init__(self, data):
+        self.id = data['id']
+        self.additional_cost = data['additional_cost']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+        @classmethod
+        def get_commission_add_ons(self, data):
+            pass
+
+        @classmethod
+        def insert_add_on(self, data):
+            pass
+
+        
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import Add_ons, Clients, Commissions, Payments, Social_medias
+from flask_app.controllers import Add_ons, Clients, Orders, Payments, Social_medias, Templates
 
 if __name__ == "__main__":
     app.run(debug=True)
